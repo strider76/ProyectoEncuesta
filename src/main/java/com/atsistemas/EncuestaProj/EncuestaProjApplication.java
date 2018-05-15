0f4ef2f76d20fe61d3cd6be04ce2886c06ad38eb
strider76 <manuel@betangible.com>
2018-05-15 23:07:18 +0200

    comit de inicio

---
+++ src/main/java/com/atsistemas/EncuestaProj/EncuestaProjApplication.java
@@ -0,0 +1,12 @@
+package com.atsistemas.EncuestaProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncuestaProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncuestaProjApplication.class, args);
+	}
+}
---
+++ src/test/java/com/atsistemas/EncuestaProj/EncuestaProjApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atsistemas.EncuestaProj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EncuestaProjApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
