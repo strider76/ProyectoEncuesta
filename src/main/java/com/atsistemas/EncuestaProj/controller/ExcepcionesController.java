4500aa36918a39604883a19be35470e700d42b21
strider76 <manuel@betangible.com>
2018-05-28 09:18:37 +0200

    answerNotFoundException

--- src/main/java/com/atsistemas/EncuestaProj/controller/ExcepcionesController.java
+++ src/main/java/com/atsistemas/EncuestaProj/controller/ExcepcionesController.java
@@ -8,7 +8,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import com.atsistemas.EncuestaProj.dto.ErrorDTO;
+import com.atsistemas.EncuestaProj.excepciones.AnswerNotFoundException;
 import com.atsistemas.EncuestaProj.excepciones.CourseNotfoundException;
+import com.atsistemas.EncuestaProj.excepciones.DificultyNotFoundException;
+import com.atsistemas.EncuestaProj.excepciones.QuestionNotFoundException;
+import com.atsistemas.EncuestaProj.excepciones.TagNotFoundException;
 import com.atsistemas.EncuestaProj.excepciones.UserNotFoundException;
 
 @ControllerAdvice(basePackages={"com.atsistemas.EncuestaProj.controller"})
@@ -30,6 +34,31 @@ public class ExcepcionesController {
 	public ResponseEntity<ErrorDTO> existeCurso(Exception ex){
 		return new ResponseEntity<ErrorDTO>(new ErrorDTO(ex.getMessage()), HttpStatus.NOT_FOUND);
 	}
+
+	@ExceptionHandler(TagNotFoundException.class)
+	public ResponseEntity<ErrorDTO> existeTag(Exception ex){
+		return new ResponseEntity<ErrorDTO>(new ErrorDTO(ex.getMessage()), HttpStatus.NOT_FOUND);
+	}	
+	
+	@ExceptionHandler(DificultyNotFoundException.class)
+	public ResponseEntity<ErrorDTO> existeDificult(Exception ex){
+		return new ResponseEntity<ErrorDTO>(new ErrorDTO(ex.getMessage()), HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(QuestionNotFoundException.class)
+	public ResponseEntity<ErrorDTO> existeQuestion(Exception ex){
+		return new ResponseEntity<ErrorDTO>(new ErrorDTO(ex.getMessage()), HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(SurveyNotFoundException.class)
+	public ResponseEntity<ErrorDTO> existeSurvey(Exception ex){
+		return new ResponseEntity<ErrorDTO>(new ErrorDTO(ex.getMessage()), HttpStatus.NOT_FOUND);
+	}	
+
+	@ExceptionHandler(AnswerNotFoundException.class)
+	public ResponseEntity<ErrorDTO> existeAnswer(Exception ex){
+		return new ResponseEntity<ErrorDTO>(new ErrorDTO(ex.getMessage()), HttpStatus.NOT_FOUND);
+	}		
 	
 	@ExceptionHandler(NumberFormatException.class)
 	public ResponseEntity<ErrorDTO> exNumerico(Exception ex, WebRequest request) {
